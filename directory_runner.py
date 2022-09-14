f245eea6542057c39e24449124d567c80d0d18b4
Matt ODonnell <matt.odonnell@leandog.com>
2022-09-14 09:49:53 -0400

    done for the morning.  can write to stdin for subproccess.  how determine if the process is waiting for input so I can write the 1/2 line in binding.pry or pdb

--- directory_runner.py
+++ directory_runner.py
@@ -7,7 +7,26 @@ import re
 import time
 import sys
 import re
+import select
 
+pass_text = '''
+
+        ██████╗  █████╗ ███████╗███████╗
+        ██╔══██╗██╔══██╗██╔════╝██╔════╝
+        ██████╔╝███████║███████╗███████╗
+        ██╔═══╝ ██╔══██║╚════██║╚════██║
+        ██║     ██║  ██║███████║███████║
+        ╚═╝     ╚═╝  ╚═╝╚══════╝╚══════╝
+'''
+fail_text = '''
+
+        ███████╗ █████╗ ██╗██╗
+        ██╔════╝██╔══██╗██║██║
+        █████╗  ███████║██║██║
+        ██╔══╝  ██╔══██║██║██║
+        ██║     ██║  ██║██║███████╗
+        ╚═╝     ╚═╝  ╚═╝╚═╝╚══════╝
+'''
 
 COMMAND_DIRECTORY = os.path.join(os.path.dirname(__file__), 'commands')
 
@@ -17,96 +36,96 @@ if not os.path.exists(COMMAND_DIRECTORY):
 def is_buffered_line(start_str, buffered):
     return start_str.startswith(buffered) or re.match(start_str, buffered)
 
-def run_command(c, directory=None):
+
+def is_stuff_to_read(p):
+    return bool(select.select([p.stdout, ], [], [], 0.0)[0])
+
+
+def write_buffer(buffered):
+    term_width = os.get_terminal_size().columns
+    buffered = re.sub("OK", "\033[42m\033[30m{}\033[00m".format("OK".ljust(term_width)), buffered )
+    buffered  = re.sub("FAILED(.*)\n", r"\033[41m\033[37m{}\1\033[00m\n".format("FAILED".ljust(term_width)), buffered )
+
+    re_text = r'(?P<total_count>[\d]+) example[s]*, (?P<failure_count>[\d]+) failure[s]*'
+    result = re.search(re_text, buffered)
+
+    if result and int(result.group("failure_count")) > 0:
+        total_match = result.group(0)
+        buffered = re.sub(total_match, "\033[41m\033[37m{}\1\033[00m".format(total_match.ljust(term_width)), buffered)
+    elif result and int(result.group("failure_count")) == 0:
+        total_match = result.group(0)
+        buffered = re.sub(total_match, "\033[42m\033[30m{}\033[00m".format(total_match.ljust(term_width)), buffered)
+
+    sys.stdout.write(buffered)
+
+
+def run_command(c, directory, conn):
+    term_width = os.get_terminal_size().columns
+
     print("running command: " + c)
     print("in directory: " + directory)
     #"stdbuf -o0 " +
     temp_c = f"bash -l '{c}'"
     temp_c = ['bash', '-lc', c]
-    p = subprocess.Popen(temp_c, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=directory, bufsize=0, )
+    p = subprocess.Popen(temp_c, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, stdin=subprocess.PIPE, cwd=directory, bufsize=0, )
     d = ""
-    data = ""
     current_line = ''
-    lines = []
-    longest_line = 0
-    buffering_line = True
     buffered = ""
 
 
     while(p.poll() is None):
+        #stuff=sys.stdin.read()
+        # stuff !='':
+        #if stuff is not None and stuff != '':
+        #sys.stdout.write(f"reading stdin: {stuff}")
+        if(conn.poll()):
+            write_buffer(buffered)
+            buffered = ''
+            stuff = conn.recv()
+            p.stdin.write(stuff.encode('utf-8'))
+
+        if(not is_stuff_to_read(p)):
+            continue
+
         character = p.stdout.read(1).decode("utf-8")
-        data += character
-        current_line += character
-        if buffering_line:
-            buffered += character
+        buffered += character
 
         if character == "\n":
-            lines.append(current_line)
-            if re.match("[-]+", current_line):
-                longest_line = max([len(current_line), longest_line])
-            current_line = ""
-            buffering_line = False
+            write_buffer(buffered)
+            buffered = ""
 
         #if not (is_buffered_line("OK", buffered) or is_buffered_line("FAILED", buffered)):
         #    buffering_line = False
 
-        if not buffering_line and len(buffered) >= 1:
-            buffered = re.sub("OK", "\033[42m\033[30m{}\033[00m".format("OK".ljust(longest_line)), buffered )
-            buffered  = re.sub("FAILED(.*)\n", r"\033[41m\033[37m{}\1\033[00m\n".format("FAILED".ljust(longest_line)), buffered )
-
-            re_text = r'(?P<total_count>[\d]+) example[s]*, (?P<failure_count>[\d]+) failure[s]*'
-            result = re.search(re_text, buffered)
-
-            #if len(buffered) == 21:
-            #    print('right line ????????????????')
-            #    print(buffered)
-            #    print(result)
-            #    print('failure' in buffered)
-            #    print('end')
-
-            if result and int(result.group("failure_count")) > 0:
-                total_match = result.group(0)
-                buffered = re.sub(total_match, "\033[41m\033[37m{}\1\033[00m".format(total_match.ljust(longest_line)), buffered)
-            elif result and int(result.group("failure_count")) == 0:
-                total_match = result.group(0)
-                buffered = re.sub(total_match, "\033[42m\033[30m{}\033[00m".format(total_match.ljust(longest_line)), buffered)
-
-            sys.stdout.write(buffered)
-            buffered = ""
-        elif not buffering_line:
-            sys.stdout.write(character)
-        sys.stdout.flush()
-
-        if character == "\n":
-            buffering_line = True
-
-        #longest_line = max([len(l) for l in data.encode("utf-8").split("\n") if re.match("[-]+", l)] + [0])
+    sys.stdout.flush()
     the_rest = p.stdout.read().decode("utf-8")
-    the_rest = re.sub("OK", "\033[42m\033[30m{}\033[00m".format("OK".ljust(longest_line)), the_rest )
-    the_rest  = re.sub("FAILED(.*)\n", r"\033[41m\033[37m{}\1\033[00m\n".format("FAILED".ljust(longest_line)), the_rest )
+    the_rest = re.sub("OK", "\033[42m\033[30m{}\033[00m".format("OK".ljust(term_width)), the_rest )
+    the_rest  = re.sub("FAILED(.*)\n", r"\033[41m\033[37m{}\1\033[00m\n".format("FAILED".ljust(term_width)), the_rest )
 
     re_text = '(?P<total_count>[\d]+) example[s]*, (?P<failure_count>[\d]+) failure[s]*'
     result = re.search(re_text, the_rest)
 
+    #stuff=conn.recv()
+    #if stuff is not None and stuff != '':
+    #sys.stdout.write(f"reading stdin: {stuff}")
+    #sys.stdout.flush()
+
 
     if result and int(result.group("failure_count")) > 0:
         total_match = result.group(0)
-        the_rest = re.sub(total_match, "\033[41m\033[37m{}\1\033[00m".format(total_match.ljust(longest_line)), the_rest)
+        the_rest = re.sub(total_match, "\033[41m\033[37m{}\1\033[00m".format(total_match.ljust(term_width)), the_rest)
     sys.stdout.write(the_rest)
     sys.stdout.flush()
 
     if bool(p.returncode):
-        sys.stdout.write("\033[41m\033[37m{}\1\033[00m".format("FAILED".ljust(longest_line)))
+        sys.stdout.write("\033[41m\033[37m{}\1\033[00m".format(fail_text))
     else:
-        sys.stdout.write("\033[42m\033[30m{}\033[00m".format("SUCCESS".ljust(longest_line)))
+        sys.stdout.write("\033[42m\033[30m{}\033[00m".format(pass_text))
     sys.stdout.flush()
-
-
-    print
 
     return ""
 
-def run_commands_if_present():
+def run_commands_if_present(conn):
     things = os.listdir(COMMAND_DIRECTORY)
     if len(things) > 0:
         for thing in things:
@@ -114,11 +133,11 @@ def run_commands_if_present():
             with open(command_file_path) as f:
                 command_info = json.loads(f.read())
                 os.remove(command_file_path)
-                run_command(command_info['command'], command_info['directory'])
+                run_command(command_info['command'], command_info['directory'], conn)
 
 
-def run_watcher_forever():
+def run_watcher_forever(conn):
     print("watching directory...")
     while True:
         time.sleep(.5)
-        run_commands_if_present()
+        run_commands_if_present(conn)
--- start_everything.py
+++ start_everything.py
@@ -2,6 +2,7 @@ import signal
 import sys
 import multiprocessing
 import time
+import select
 
 import directory_runner
 import server
@@ -15,14 +16,36 @@ def handle_signal(signum, frame):
 
 signal.signal(signal.SIGINT, handle_signal)
 
-processes.append(multiprocessing.Process(target=directory_runner.run_watcher_forever))
+parent_conn, child_conn = multiprocessing.Pipe()
+
+processes.append(multiprocessing.Process(target=directory_runner.run_watcher_forever, args=(child_conn,)))
 processes.append(multiprocessing.Process(target=server.run_server_forever))
 
+print('starting everything')
 for process in processes:
     process.start()
 
+def is_stuff_to_read():
+    return bool(select.select([sys.stdin, ], [], [], 0.0)[0])
+
 while should_be_running:
     time.sleep(1)
+    if(is_stuff_to_read()):
+        while(is_stuff_to_read()):
+            parent_conn.send(sys.stdin.readline())
+            print("stuff to read")
+    #i = sys.stdin.read()
+    #if i != '':
+    #    print("Found stuff")
+    #    print(i)
+    #    parent_conn.send(i)
+
+
+#print("exiting...")
+#for i in range(1,10):
+#    time.sleep(.5)
+#    print(select.select([sys.stdin, ], [], [], 0.0)[0])
+#print('done reading lines')
 
 for process in processes:
     process.terminate()
